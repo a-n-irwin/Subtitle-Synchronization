e2d77ccdccd790d2b66a8e181a8f9b2d1d7976f9
Irwin <developer.irwin@gmail.com>
2024-09-02 18:16:05 +0100

    Add files via upload
    Version update

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using SubtitleSynchronization;
+
+
+public static class Program
+{
+    public static void Main()
+    {
+        var path = "sub.txt";
+        SRTSynchronizer sync = new SRTSynchronizer();
+        SynchronizationData data = sync.SyncByMilliseconds(path, 1000);
+        Console.WriteLine(data);
+    }
+}
